666935df2445e8b98f7dc13b54b22bb4a603018d
ppoiuy <xxoxooxox@gmail.com>
2017-05-24 20:51:58 +0800

    add TYPE class to stubs

---
+++ stubs/TYPE.java
@@ -0,0 +1,13 @@
+package stubs;
+
+public final class TYPE {
+
+	public final static int number = 4;
+
+	public final static SeatType SFC = new SeatType("SFC", 100000, 1, 3);
+	public final static SeatType SBC = new SeatType("SBC", 50000, 6, 13);
+	public final static SeatType EDC = new SeatType("EDC", 20000, 27, 41);
+	public final static SeatType EC = new SeatType("EC", 5000, 45, 70);
+
+
+}
