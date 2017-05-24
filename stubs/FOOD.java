b90f9bb5686337e7bc9612caefe06fba5f7f2e41
ppoiuy <xxoxooxox@gmail.com>
2017-05-24 20:52:51 +0800

    add FOOD class to stubs

---
+++ stubs/FOOD.java
@@ -0,0 +1,12 @@
+package stubs;
+
+public final class FOOD {
+
+	public final static int number = 4;
+
+	public final static FoodOrder BEEF = new FoodOrder("Beef", 2000);
+	public final static FoodOrder PORK = new FoodOrder("Pork", 1000);
+	public final static FoodOrder VEGETABLE = new FoodOrder("Vegetable", 500);
+	public final static FoodOrder NONE = new FoodOrder("None", 0);
+
+}
